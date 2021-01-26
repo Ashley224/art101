7eda3459d604c8852cd07c517449e5c544314c07
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-01-25 22:02:20 -0800

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Loop until we find all the trash
+//    Search the desk for trash
+//    Pick up the trash and put it in the trash bin
+// Loop until everything is off the desk
+//    Pick up one item from the desk
+//    Move the item to another place
+// Wipe the desk clean
+// Loop until everything is organized on the desk
+//    Pick up an item from the other place
+//    Place the item on the desk
+// Loop until we find every item we need for later
+//    Look for an item we are going to use later
+//    Organize the item on the desk
