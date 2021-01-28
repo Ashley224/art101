41f47b70623e7b88e0546f7d8c858d1fa39a3388
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-01-27 20:11:46 -0800

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Yuhao Chen <ychen514@ucsc.edu
+ * Created: 28 January
+ * License: Public Domain
+ */
+
+//Define Variables
+make = "Toyota";
+model = "Camry";
+color = "Supersonic Red";
+year = 2019;
+
+//Calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
