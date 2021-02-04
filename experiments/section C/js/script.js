a69a256494de72bdb5e862fdb56458496a272692
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-02-03 19:32:32 -0800

    Create script.js

---
+++ experiments/section C/js/script.js
@@ -0,0 +1,12 @@
+function firstFunction(){
+	console.log("this is my first function");
+	console.log(numNumb);
+	console.log(nameVar);
+}
+function testBigger(firstNum,secondNum){
+	console.log("First Number: "+firstNum+" Second Number: "+secondNum+" comparison:" + (firstNum>secondNum) );
+}
+var nameVar = "Toni";
+var numNUmb = 17;
+//firstFunction();
+testBigger(5,8);
