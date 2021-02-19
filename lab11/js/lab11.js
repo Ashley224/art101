d1e60e6649536edbb66ca00b0ae4807b75234a61
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-02-18 16:18:59 -0800

    Update lab11.js

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -3,6 +3,33 @@
  * Created: 17 February
  * License: Public Domain
  */
- $(document).ready(function(){
- $(":button").css("background-color", "red");
- });
+
+$("#challenge").append("<button id=cb>challenge");
+
+$("#problem").append("<button id=pb>problem");
+
+$("#result").append("<button id=rb>result");
+
+$("#cb").click(function () {
+  alert("Go away.");
+  $("#output").append("<p>challenge button is pushed</p>");
+  $("#pb").css("color", "green");
+  $("#cb").css("color", "black");
+});
+
+$("#pb").click(function () {
+  alert("Stop!!!");
+  $("#output").append("<p>problem button is pushed</p>");
+  $("#rb").css("color", "blue");
+  $("#pb").css("color", "black");
+});
+
+$("#rb").click(function () {
+  alert("Stop it already!");
+  $("#output").append("<p>result button is pushed</p>");
+  $("#cb").css("color", "yellow");
+  $("#rb").css("color", "black");
+});
+
+
+
