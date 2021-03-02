10aa357739dddedbc75f639bb8e6c06b107bc797
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 18:56:19 -0800

    Update lab13.js

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -5,6 +5,21 @@
  */
 
  function fizzBuzzBoom() {
- 	var output;
- 	far (var i = 1; i <= 200; )
- }
+  var output;
+  for (var i = 1; i <= 200; i += 1) {
+    output += i + ": ";
+    if (i % 3 == 0) {
+      output += "Fizz!";
+    }
+    if (i % 5 == 0) {
+      output += "Buzz!";
+    }
+    if (i % 7 == 0) {
+      output += "Boom!";
+    }
+    output += "<br>";
+  }
+  return output;
+}
+
+document.getElementById("output").innerHTML = fizzBuzzBoom();
