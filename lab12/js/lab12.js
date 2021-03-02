42cee9179fb1aef2ed2aa5c68c8bb42fae79d531
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 18:47:10 -0800

    Update lab12.js

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -4,16 +4,21 @@
  * License: Public Domain
  */
  
+ // Return Gryffindor, Ravenclaw,Slytherin, and Hufflepuff
+ // depending on length mod 4
 function sortingHat(str) {
   len = str.length;
   mod = len % 4;
   if (mod == 0) {
     return "Gryffindor";
-  } else if (mod == 1) {
+  } 
+  else if (mod == 1) {
     return "Ravenclaw";
-  } else if (mod == 2) {
+  } 
+  else if (mod == 2) {
     return "Slytherin";
-  } else if (mod == 3) {
+  } 
+  else if (mod == 3) {
     return "Hufflepuff";
   }
 }
