0d9871b5a3da9ae3420a78d44a414ec77f0c9b89
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 17:25:16 -0800

    Update lab12.js

---
+++ lab12/js/lab12.js
@@ -0,0 +1,30 @@
+/*
+ * Author: Ashley Mojica <asmojica@ucsc.edu>
+ * Created: 17 February
+ * License: Public Domain
+ */
+ 
+ function sortingHat(str) {
+ 	len = str.length;
+ 	mod = len % 4;
+ 	if (mod == 0) {
+ 		return "Gryffindor";
+ 	}
+ 	else if (mod == 1) {
+ 		return "Ravenclaw";
+ 	}
+ 	else if (mod == 2) {
+ 		return "Slytherin";
+ 	}
+ 	else if (mod == 3) {
+ 		return "Hufflepuff";
+ 	}
+ }
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+	var name = document.getElementById("input").value;
+	var house = sortingHate(name);
+	newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+	document.getElementById("output").innerHTML = newText;
+})
