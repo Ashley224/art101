9275a47217179cbe43480e1618ad1cc2373c9b72
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 18:37:58 -0800

    Update lab12.js

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -4,22 +4,27 @@
  * License: Public Domain
  */
  
- function sortingHat(str) {
- 	len = str.length;
- 	mod = len % 4;
- 	if (mod == 0) {
- 		return "Gryffindor";
- 	}
- 	else if (mod == 1) {
- 		return "Ravenclaw";
- 	}
- 	else if (mod == 2) {
- 		return "Slytherin";
- 	}
- 	else if (mod == 3) {
- 		return "Hufflepuff";
- 	}
- }
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor";
+  } else if (mod == 1) {
+    return "Ravenclaw";
+  } else if (mod == 2) {
+    return "Slytherin";
+  } else if (mod == 3) {
+    return "Hufflepuff";
+  }
+}
+
+var myButtton = document.getElementById("button");
+myButtton.addEventListener("click", function () {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+});
 
 var myButton = document.getElementById("button");
 myButton.addEventListener("click", function() {
