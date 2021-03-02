c4a94ff4bdf82f85a88fe6a9fdc67c47d2049237
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 17:24:43 -0800

    Create lab13.js

---
+++ lab13/js/lab13.js
@@ -0,0 +1,10 @@
+/*
+ * Author: Ashley Mojica <asmojica@ucsc.edu>
+ * Created: 17 February
+ * License: Public Domain
+ */
+
+ function fizzBuzzBoom() {
+ 	var output;
+ 	far (var i = 1; i <= 200; )
+ }
