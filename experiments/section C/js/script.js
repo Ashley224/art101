fb71e32ce93a1f7a075d39ecb92e25fcf62e70cc
Ashley224 <77122118+Ashley224@users.noreply.github.com>
2021-03-01 17:25:35 -0800

    Update script.js

--- experiments/section C/js/script.js
+++ experiments/section C/js/script.js
@@ -1,12 +1,23 @@
-function firstFunction(){
-	console.log("this is my first function");
-	console.log(numNumb);
-	console.log(nameVar);
+//keypress
+//substring
+//change border color when text is entered
+
+var orignial_text = $("#orignal_text p").text();
+console.log(orignal_text);
+
+function matchText(){
+  var input_text = $("#input_text").val();
+  var substring_match = orignal_text.substring(0,input_text.length)
+  console.log(substring_match);
+  if (input_text==substring_match){
+    $("#input_text").css("border-color","blue");
+  }else{
+    $("#input_text").css("border-color", "red");
+  }
+  //3 conditions
+  //text matching before finishing blue
+  //text not matching red
+  //text matching total green
+
 }
-function testBigger(firstNum,secondNum){
-	console.log("First Number: "+firstNum+" Second Number: "+secondNum+" comparison:" + (firstNum>secondNum) );
-}
-var nameVar = "Toni";
-var numNUmb = 17;
-//firstFunction();
-testBigger(5,8);
+$("#input_text").keypress(matchText);
